36fb4c87e47048528c63e0d568ada08e5f014219
Jean Carlos Lerma Rojas <yancalerma@gmail.com>
2023-12-10 12:06:21 -0500

    Solucion ingenua construccion

--- src/main/java/org/ada/Controllers/IngenuousSolutionController.java
+++ src/main/java/org/ada/Controllers/IngenuousSolutionController.java
@@ -1,6 +1,8 @@
 package org.ada.Controllers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 public class IngenuousSolutionController {
@@ -19,39 +21,72 @@ public class IngenuousSolutionController {
     }
 
     public int[][] createCalendarSolution(){
+        int[][] calendarSolution = new int[2 * (teams - 1)][teams];
         int[][] firstPart = new int[teams - 1][teams];
+        int[][] secondPart =  new int[teams - 1][teams];
+        int[] numbers = new int[teams];
+        Arrays.fill(numbers, 1);
 
-        for(int i = 0; i < teams - 1; i++){
-            int[] row = new int[teams];
 
-            int count = 0;
-            while(count < teams){
-                int numberRandom = random.nextInt(4) + 1;
-                boolean validateNumber = false;
+        for(int j = 0; j < firstPart[0].length; j++){
+            int countPositive = 1;
+            int countNegative = 1;
+            int controladorSigno = 1;
 
-                int countValidate = 0;
-                while(countValidate != i){
-                    int before = firstPart[countValidate][count];
+            for(int i = 0; i < firstPart.length; i++){
+                if(firstPart[i][j] == 0){
+                    ArrayList<Integer> repeats = this.findRowsAndColumns(firstPart, i, j);
 
-                    if(numberRandom == before){
-                        numberRandom = random.nextInt(4) + 1;
-                        countValidate = 0;
+                    for(int indexNumbers = 0; indexNumbers < numbers.length; indexNumbers++){
+                        if(numbers[indexNumbers] != 0 && indexNumbers != j && !repeats.contains(indexNumbers + 1)){
+
+                            if(!(min <= (countPositive) && countPositive <= max ) || !((countNegative) >= min && countNegative <= max)){
+                                controladorSigno = controladorSigno * -1;
+
+                                if(countPositive == max) countPositive = 1;
+                                if(countNegative == max) countNegative = 1;
+                            }
+
+                            int numberSelected = (indexNumbers + 1) * controladorSigno;
+
+                            firstPart[i][j] = numberSelected;
+                            firstPart[i][indexNumbers] = (j + 1) * -1 * controladorSigno ;
+
+
+                            secondPart[i][j] = numberSelected * -1;
+                            secondPart[i][indexNumbers] = (j + 1) * controladorSigno;
+                            numbers[indexNumbers]--;
+
+                            if(numberSelected > 0) countPositive++;
+                            if(numberSelected < 0) countNegative++;
+                            break;
+                        }
                     }
 
-                    if((count + 1 ) == numberRandom){
-                        numberRandom = random.nextInt(4) + 1;
-                        countValidate = 0;
-                    }
-                    countValidate++;
+                    Arrays.fill(numbers, 1);
                 }
-
-                row[count] = numberRandom;
-                count++;
             }
-
-
-            firstPart[i] = row;
         }
+
+        System.arraycopy(firstPart, 0, calendarSolution, 0, firstPart.length);
+        System.arraycopy(secondPart, 0, calendarSolution, firstPart.length, secondPart.length);
+
+        /*int countPositive = 0;
+        int countNegative = 0;
+        int index = 0;
+        for(int[] row: calendarSolution){
+            if(row[0] > 0) countPositive++;
+            if(row[0] < 0) countNegative++;
+
+            if(countPositive == max || countNegative == max){
+                int[] currentRow = row;
+                int
+            }
+        }*/
+        for(int[] row: calendarSolution){
+            System.out.println(Arrays.toString(row));
+        }
+
        return  firstPart;
     }
 
@@ -62,4 +97,30 @@ public class IngenuousSolutionController {
     public int[][] getMatrixSolution(){
         return calendarSolution;
     };
+
+    private ArrayList<Integer> findRowsAndColumns (int[][] A, int row, int column){
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int[] ints : A) {
+            int number = ints[column];
+            if(number < 0){
+                number = number * -1;
+            }
+            result.add(number);
+        }
+
+        for(int j = 0; j < A[0].length; j++){
+            int number = A[row][j];
+            if(number < 0){
+                number = number * -1;
+            }
+            result.add(number);
+        }
+
+        return  result;
+    }
+
+    private int countPermanecia(int[][] A, int column){
+        return 0;
+    }
 }
--- src/main/java/org/ada/Main.java
+++ src/main/java/org/ada/Main.java
@@ -7,22 +7,26 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int team = 4;
+        int team = 8;
         int min = 1;
-        int max = 3;
+        int max = 6;
         int[][] distance = {
-            {0, 745, 665, 929},
-            {745, 0, 80, 337},
-            {665, 80, 0, 380},
-            {929, 337, 380, 0}
+            {0, 745, 665, 929, 0, 745, 665, 929,},
+            {745, 0, 80, 337, 665, 80, 0, 380},
+            {665, 80, 0, 380, 929, 337, 380, 0},
+            {929, 337, 380, 0, 0, 745, 665, 929},
+            {0, 745, 665, 929, 0, 745, 665, 929,},
+            {745, 0, 80, 337, 665, 80, 0, 380},
+            {665, 80, 0, 380, 929, 337, 380, 0},
+            {929, 337, 380, 0, 0, 745, 665, 929}
         };
         IngenuousSolutionController controller = new IngenuousSolutionController(team, distance, min, max);
         int [][] calendarSolution = controller.createCalendarSolution();
 
-        System.out.println("Matriz soliucion");
+        /*System.out.println("Matriz soliucion");
         for(int[] row: calendarSolution){
             System.out.println(Arrays.toString(row));
-        }
+        }*/
         //new IndexView();
     }
 }
